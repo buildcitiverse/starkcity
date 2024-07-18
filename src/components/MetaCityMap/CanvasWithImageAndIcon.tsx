a6a58aad39a81ce18e927fc832927391b8bc4608
ssm-tungna <tungna@saseeme.com>
2024-07-18 15:45:38 +0700

    refactor code

--- src/components/MetaCityMap/CanvasWithImageAndIcon.tsx
+++ src/components/MetaCityMap/CanvasWithImageAndIcon.tsx
@@ -4,11 +4,9 @@ import { Box, Button, Flex } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 import { selectedItemData } from "@/src/redux/slices/selectedItemSlice";
 import ButtonMap from "./ButtonMap";
+import { Point, originalPoints } from "./configMapCity";
 
-interface Point {
-  x: number;
-  y: number;
-}
+
 
 const CanvasWithImageAndIcon: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -29,69 +27,6 @@ const CanvasWithImageAndIcon: React.FC = () => {
   const [panPosition, setPanPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
   const [isDragging, setIsDragging] = useState(false);
   const [dragStart, setDragStart] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-
-  const originalPoints: Point[] = [
-    { x: 0.674, y: 0.27 },  // 1
-    { x: 0.614, y: 0.172 }, // 2
-    { x: 0.533, y: 0.118 }, // 3
-    { x: 0.441, y: 0.123 }, // 4
-    { x: 0.365, y: 0.185 }, // 5
-    { x: 0.308, y: 0.292 }, // 6
-    { x: 0.276, y: 0.43 },  // 7
-    { x: 0.275, y: 0.58 },  // 8
-    { x: 0.305, y: 0.718 }, // 9
-    { x: 0.362, y: 0.827 }, // 10
-    { x: 0.437, y: 0.892 }, // 11
-    { x: 0.538, y: 0.898 }, // 12
-    { x: 0.616, y: 0.842 }, // 13
-    { x: 0.677, y: 0.738 }, // 14
-    { x: 0.712, y: 0.603 },  // 15
-    { x: 0.386, y: 0.27 }, // 16
-    { x: 0.428, y: 0.241 },  // 17
-    { x: 0.452, y: 0.241 },  // 18
-    { x: 0.475, y: 0.241 },   // 19
-    { x: 0.427, y: 0.371 }, // 20
-    { x: 0.324, y: 0.528 },  // 21
-    { x: 0.356, y: 0.484 }, // 22
-    { x: 0.356, y: 0.52 }, // 23
-    { x: 0.356, y: 0.555 }, // 24
-    { x: 0.397, y: 0.484 }, // 25
-    { x: 0.397, y: 0.484 }, // 26
-    { x: 0.397, y: 0.555 }, // 27
-    { x: 0.437, y: 0.484 }, // 28
-    { x: 0.437, y: 0.52 }, // 29
-    { x: 0.437, y: 0.555 }, // 30
-    { x: 0.476, y: 0.484 }, // 31
-    { x: 0.476, y: 0.52 }, // 32
-    { x: 0.476, y: 0.555 }, // 33
-    { x: 0.343, y: 0.629 },  // 34
-    { x: 0.397, y: 0.624 }, // 35
-    { x: 0.397, y: 0.661 }, // 36
-    { x: 0.397, y: 0.698 }, // 37
-    { x: 0.437, y: 0.624 }, // 38
-    { x: 0.437, y: 0.661 }, // 39
-    { x: 0.437, y: 0.698 }, // 40
-    { x: 0.476, y: 0.624 }, // 41
-    { x: 0.476, y: 0.661 }, // 42
-    { x: 0.476, y: 0.698 }, // 43
-    { x: 0.399, y: 0.744 }, // 44
-    { x: 0.434, y: 0.77 },  // 45
-    { x: 0.475, y: 0.81 },    // 46
-    { x: 0.538, y: 0.468 }, // 47
-    { x: 0.573, y: 0.468 },  // 48
-    { x: 0.608, y: 0.468 },  // 49
-    { x: 0.599, y: 0.567 }, // 50
-    { x: 0.56, y: 0.573 },  // 51
-    { x: 0.532, y: 0.618 }, // 52
-    { x: 0.525, y: 0.679 }, // 53
-    { x: 0.539, y: 0.737 }, // 54
-    { x: 0.572, y: 0.772 }, // 55
-    { x: 0.611, y: 0.776 }, // 56
-    { x: 0.646, y: 0.743 }, // 57
-    { x: 0.663, y: 0.688 }, // 58
-    { x: 0.659, y: 0.627 }, // 59
-    { x: 0.634, y: 0.578 }, // 60
-  ];
 
   const [iconPosition, setIconPosition] = useState<Point>(originalPoints[0]);
   const iconRef = useRef<HTMLImageElement | null>(null);
