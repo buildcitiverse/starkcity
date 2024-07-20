dd617f9039643cb5ef6f648cb965ec22f23401d1
ssm-kiendo <kiendo@saseeme.com>
2024-07-20 10:43:39 +0700

    Kien.PN-329 Fix font chu

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,47 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-};
+    output: 'standalone',
+
+    async rewrites() {
+      return [
+        {
+          source: '/videos/:path*',
+          destination: '/api/videos/:path*',
+        },
+      ];
+    },
+
+    async headers() {
+      return [
+        {
+          source: "/assets/:all*",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, immutable, max-age=31536000",
+            },
+          ],
+        },
+      ];
+    },
+
+    images: {
+      formats:['image/avif', 'image/webp'],
+      remotePatterns:[
+        {
+          protocol:'https',
+          hostname:'dev-stark.citiverse.io',
+          port:'',
+          pathname:'/assets/**'
+        }
+      ]
+    }
+  
+  };
 
 export default nextConfig;
-
